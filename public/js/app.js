ff02469766d3e2978fa30ba044825068d8efc2fb
Michał Bociek <mic.bociek@gmail.com>
2020-10-31 19:50:34 +0100

    Setup app for HEroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', e => {
     firstParagraph.textContent = 'Loading..'
     secondParagraph.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+    fetch('/weather?address=' + location).then((res) => {
         res.json().then((data) => {
             if (data.error) {
                 firstParagraph.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define path sor Express config
 const publicDirectory = path.join(__dirname, '../public')
@@ -99,6 +100,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000")
+app.listen(port, () => {
+    console.log("Server listening on " + port)
 });
